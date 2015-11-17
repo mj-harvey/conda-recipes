7d034f6225953ad084825bdd761867f0e5c5367c
M J Harvey <m.j.harvey@acellera.com>
2015-11-17 16:27:41 +0100

    fix for multiple python versions

--- nglview/build.sh
+++ nglview/build.sh
@@ -1,1 +1,14 @@
-python setup.py install
+mkdir -p "$SP_DIR"
+
+SRC=nglview
+FIRSTVER=""
+for VER in 3.4 3.5; do
+	DIR="$SP_DIR/../../python$VER/site-packages"
+	mkdir -p $DIR
+	if [ "$FIRSTVER" == "" ]; then
+		cp -r $SRC "$DIR"
+		FIRSTVER=$VER
+	else
+		ln -s ../../python$FIRSTVER/site-packages/$SRC "$DIR/$SRC"
+	fi
+done
