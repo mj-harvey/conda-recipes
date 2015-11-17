1b735ec1c7a664c10b74321c92363774cf662e74
Gianni De Fabritiis <g.defabritiis@gmail.com>
2015-11-17 15:52:58 +0100

    merge

--- htmdbabel/build.sh
+++ htmdbabel/build.sh
@@ -1,17 +1,30 @@
 mkdir build
 cd build
+pwd
 
 PYTHONDIR="$(dirname $(which python))/../"
 
+printenv
+SUFFIX=so
+if [ "$OSX_ARCH" != "" ]; then
+	SUFFIX="dylib"
+fi
+echo $SUFFIX
+
+touch "$PREFIX"/bin/roundtrip
+
 cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-  -DPYTHON_BINDINGS=on \
-  -DRUN_SWIG=on \
-  -DPYTHON_INCLUDE_DIR="$PYTHONDIR/include/python3.4m/" \
-  -DPYTHON_LIBRARIES="$PYTHONDIR/lib/libpython3.so" \
-  -DZLIB_LIBRARY="$PYTHONDIR/lib/libz.so" \
+  -DPYTHON_BINDINGS=off \
+  -DZLIB_LIBRARY="$PYTHONDIR/lib/libz.$SUFFIX" \
   -DBUILD_GUI=off \
   -DBUILD_SHARED=off \
-  -DBUILD_MIXED=on 
+  -DBUILD_MIXED=on  \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET="" \
+  -DENABLE_TESTS=off \
+#  -DRUN_SWIG=on \
+#  -DPYTHON_INCLUDE_DIR="$PYTHONDIR/include/python3.4m/" \
+#  -DPYTHON_LIBRARIES="$PYTHONDIR/lib/libpython3.$SUFFIX" \
+
 
 printenv
 
