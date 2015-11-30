d9f33b1f3d2d3e766d69ef6de77f65ef0b1e270f
MJ Harvey <m.j.harvey@acellera.com>
2015-11-30 18:43:13 +0000

    rev mklrt

--- mklrt/build.sh
+++ mklrt/build.sh
@@ -3,6 +3,8 @@
 D="$SP_DIR/../../../"
 mkdir -p "$D/lib"
 
+cp -p  /apps/intel/2015.3/composer_xe_2015.3.187/compiler/lib/intel64/libintlc.so.5 "$D/lib"
 cp -r  /apps/intel/2015.3//lib/intel64/libi*.so "$D/lib/"
+cp -r  /apps/intel/2015.3//lib/intel64/libsvml.so "$D/lib/"
 cp -r  /apps/intel/2015.3/mkl/lib/intel64/*.so "$D/lib/"
 
