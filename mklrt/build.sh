cbed18ed144b25fa959601702cf2c7c819692222
MJ Harvey <m.j.harvey@acellera.com>
2015-11-30 18:40:29 +0000

    rev mklrt

--- mklrt/build.sh
+++ mklrt/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 D="$SP_DIR/../../../"
-mkdir -p $D/lib
+mkdir -p "$D/lib"
 
-cp -r $HOME/psi4/lib/* $D/lib/
+cp -r  /apps/intel/2015.3//lib/intel64/libi*.so "$D/lib/"
+cp -r  /apps/intel/2015.3/mkl/lib/intel64/*.so "$D/lib/"
 
