66b273c7afa96d3571dc16eceb7aaa25643e8318
MJH <m.j.harvey@acellera.com>
2015-12-04 14:35:33 +0100

    boto 3 packages

---
+++ jmespath/build.sh
@@ -0,0 +1,14 @@
+mkdir -p "$SP_DIR"
+
+SRC=jmespath
+FIRSTVER=""
+for VER in 3.4 3.5; do
+	DIR="$SP_DIR/../../python$VER/site-packages"
+	mkdir -p $DIR
+	if [ "$FIRSTVER" == "" ]; then
+		cp -r $SRC "$DIR"
+		FIRSTVER=$VER
+	else
+		ln -s ../../python$FIRSTVER/site-packages/$SRC "$DIR/$SRC"
+	fi
+done
