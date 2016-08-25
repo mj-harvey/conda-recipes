f1161c7f24dea15cde99466e0bc6ad569361d4e6
MJ Harvey <m.j.harvey@acellera.com>
2016-08-25 21:38:09 +0100

    boinc

--- boinc/build.sh
+++ boinc/build.sh
@@ -1,20 +1,25 @@
-if [ ! -e "repo/boinc" ]; then
-	mkdir -p repo
-	cd repo
-	git clone --depth 1 https://github.com/BOINC/boinc
-  cd ..
-fi
-pwd
-cd repo/boinc
-git pull
-if [ ! -e "configure" ]; then
-	./_autosetup
-fi
-if [ ! -e config.log ]; then
-	./configure --prefix "$SP_DIR" --disable-server --disable-manager --disable-fcgi  --disable-client --enable-shared LDFLAGS="-lssl -lcrypto" CFLAGS="-fPIC" CXXFLAGS="-fPIC"
-fi
-make
-echo "Installing into $SP_DIR"
-gcc --shared -o "$SP_DIR/boinc/libboinc.so" lib/*.o
-cp -r ../../boinc "$SP_DIR"
+#XX="$PWD"
+#
+#if [ ! -e "/tmp/repo/boinc" ]; then
+#	mkdir -p /tmp/repo
+#	cd /tmp/repo
+#	git clone --depth 1 https://github.com/BOINC/boinc
+#  cd ..
+#fi
+#pwd
+#cd /tmp/repo/boinc
+#git pull
+#if [ ! -e "configure" ]; then
+#  echo "x"
+#	./_autosetup
+#fi
+#if [ ! -e config.log ]; then
+#  echo "x"
+#	./configure --prefix "$SP_DIR" --disable-server --disable-manager --disable-fcgi  --disable-client --enable-shared LDFLAGS="-lssl -lcrypto" CFLAGS="-fPIC" CXXFLAGS="-fPIC"
+#fi
+#make
+#echo "Installing into $SP_DIR"
+#mkdir -p "$SP_DIR/boinc"
+#gcc --shared -o "$SP_DIR/boinc/libboinc.so" lib/*.o
+cp -r boinc "$SP_DIR"
 
