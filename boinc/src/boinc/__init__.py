2c99c67c402a1c058a5da279f49953fe476633cb
M J Harvey <moonbug1970@gmail.com>
2016-08-25 22:22:02 +0100

    boinc

--- boinc/src/boinc/__init__.py
+++ boinc/src/boinc/__init__.py
@@ -6,10 +6,10 @@ class Boinc:
 	def __init__( self ):
 			# do init
 			loc = os.path.dirname( inspect.getfile( Boinc )) 
-			lib = os.path.join( loc, "libboinc_api.so" )
+			lib = os.path.join( loc, "libboinc.so" )
 			self._lib = cdll.LoadLibrary( lib )
 			if not self._lib:
-				raise ValueError( "Could not load _boinc.so" )
+				raise ValueError( "Could not load BOINC DSO" )
 			self._init               = self._lib.boinc_init
 			self._resolve_filename   = self._lib.boinc_resolve_filename
 			self._time_to_checkpoint = self._lib.boinc_time_to_checkpoint
@@ -18,9 +18,13 @@ class Boinc:
 			self._end_critical_section = self._lib.boinc_end_critical_section
 			self._fraction_done        = self._lib.boinc_fraction_done
 			self._is_standalone        = self._lib.boinc_is_standalone
-			self._temporary_exit       = self._lib.boinc_temporary_exit
-			self._finish_message       = self._lib.boinc_finish_message
+#			self._temporary_exit       = self._lib.boinc_temporary_exit
+#			self._finish_message       = self._lib.boinc_finish_message
+			self._finish               = self._lib.boinc_finish
+			self._init()       
 
+	def finish( self, status=0 ):
+		self._finish( c_int(status) )
 
 	def resolve_filname( self, logical ):
 		c = create_string_buffer( 1025 )
@@ -49,9 +53,9 @@ class Boinc:
 		ret = self._is_standalone()
 		return (ret[0]!=0)
 
-	def temporary_exit( self, delay=0, reason=None, is_notice=False ):
-		self._temporary_exit( c_int(delay), c_char_p(reason), c_bool(is_notice) )
-		pass
+#	def temporary_exit( self, delay=0, reason=None, is_notice=False ):
+#		self._temporary_exit( c_int(delay), c_char_p(reason), c_bool(is_notice) )
+#		pass
 
-	def finish( self, status, reason=None, is_notice=False ):
-		self._finish_message( c_int(status), c_char_p( reason ), c_bool( is_notice ) )
+#	def finish( self, status, reason=None, is_notice=False ):
+#		self._finish_message( c_int(status), c_char_p( reason ), c_bool( is_notice ) )
